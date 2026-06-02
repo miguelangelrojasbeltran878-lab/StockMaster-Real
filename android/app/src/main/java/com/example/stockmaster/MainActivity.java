0cf26d9d5f8d472af6183b7736b738821eb965f7
miguelangelrojasbeltran878-lab <miguelangelrojasbeltran878@gmail.com>
2026-06-02 10:07:52 -0500

    Create MainActivity for Flutter application

---
+++ android/app/src/main/java/com/example/stockmaster/MainActivity.java
@@ -0,0 +1,11 @@
+package com.example.stockmaster;
+
+import android.os.Bundle;
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+    @override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
