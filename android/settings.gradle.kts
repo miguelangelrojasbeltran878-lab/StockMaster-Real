c47956bb05734b2c68029b5cc062ea749ef3cef1
miguelangelrojasbeltran878-lab <miguelangelrojasbeltran878@gmail.com>
2026-06-01 23:04:31 -0500

    Remove Kotlin plugin from Gradle settings
    Removed Kotlin plugin version from settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -23,7 +23,6 @@ pluginManagement {
 plugins {
     id("dev.flutter.flutter-gradle-plugin") version "1.0.0" apply false
     id("com.android.application") version "8.7.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
     id("com.google.gms.google-services") version "4.4.1" apply false
 }
 
