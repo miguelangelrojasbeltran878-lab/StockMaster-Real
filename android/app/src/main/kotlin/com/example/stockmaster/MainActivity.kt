540fd633c57cfdb473bf91ac2f8625b8bba74ed0
miguelangelrojasbeltran878-lab <miguelangelrojasbeltran878@gmail.com>
2026-06-01 22:55:07 -0500

    Simplify MainActivity by removing onCreate method

--- android/app/src/main/kotlin/com/example/stockmaster/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/stockmaster/MainActivity.kt
@@ -1,10 +1,5 @@
 package com.example.stockmaster
 
-import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-}
+class MainActivity: FlutterActivity()
